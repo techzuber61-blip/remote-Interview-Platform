f0440a7b9a027e027b4df680773ba2aec73433a8
Zuber <techzuber61@gmail.com>
2026-02-28 22:42:38 +0530

    package.json fix for deployment

---
+++ backend/src/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    profileImage: { type: String, required: true, default: "" },
+    clerkId: { type: String, required: true, unique: true },
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,19 @@ import express from 'express'
 import { ENV } from './lib/env.js'
 import path from "path"
 import { connectDB } from './lib/db.js'
+import cors from "cors"
+import { serve } from 'inngest/express'
+import { inngest } from './lib/inngest.js'
 
 const app = express()
 
 const __dirname = path.resolve()
+
+// middleware
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL, credentials: true}))
+
+app.use("/api/inngest", serve({client: inngest, functions}))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
