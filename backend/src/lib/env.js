1140479e389d08be632e8d54c902f8c64e8f4a51
Zuber <techzuber61@gmail.com>
2026-03-02 03:42:28 +0530

    stream auth and stream sync

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -8,7 +8,7 @@ export const ENV = {
     NODE_ENV: process.env.NODE_ENV,
     CLIENT_URL: process.env.CLIENT_URL,
     INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
-    INNGEST_SIGNIN_KEY: process.env.INNGEST_SIGNIN_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNIN_KEY,
     STREAM_API_KEY: process.env.STREAM_API_KEY,
     STREAM_API_SECRET: process.env.STREAM_API_SECRET,
     CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
